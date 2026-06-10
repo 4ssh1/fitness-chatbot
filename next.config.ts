6e47b509ae7fb77d5eb165ac860a1e0b72e93792
Nzeshi Sarah <sarahnzeshi05@gmail.com>
2026-06-10 23:26:27 +0100

    feat: add Docker support with Dockerfile, docker-compose, and .dockerignore; update README for building and running instructions

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  output: "standalone", // to copy exact files needed for production
 };
 
 export default nextConfig;
