77ec93afbf4e9d411f4897ac9f3f7af65ae0a447
Nzeshi Sarah <sarahnzeshi05@gmail.com>
2026-06-11 21:48:55 +0100

    Revert "feat: add Docker support with Dockerfile, docker-compose, and .dockerignore; update README for building and running instructions"
    This reverts commit 6e47b509ae7fb77d5eb165ac860a1e0b72e93792.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  output: "standalone", // to copy exact files needed for production
 };
 
 export default nextConfig;
